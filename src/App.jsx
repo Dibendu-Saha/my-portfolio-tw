6bed620e9ad65decd71687c5f1db395d651d9afa
Dibendu Saha <dibendu4@gmail.com>
2024-08-06 22:05:14 +0530

    Add height & responsiveness
    + Replace min-height with height.
    + Add responsiveness for 2nd column texts.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.scss";
 
 const App = () => {
   return (
-    <div className="mx-auto my-0 min-h-screen bg-gradient-to-r from-gray-200 to-yellow-300 p-20">
+    <div className="mx-auto my-0 h-screen bg-gradient-to-r from-gray-200 to-yellow-300 p-20">
       {/* <div className="h-screen w-full border border-red-600"></div>
       <div className="h-screen w-full border border-green-700"></div> */}
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import { component_type } from "../common/component-type";
 
 const Home = () => {
   return (
-    <div className="grid min-h-screen grid-cols-2 justify-items-center">
+    <div className="grid h-full grid-cols-2 justify-items-center border border-black">
       <div>
         <Header>Hey there!</Header>
         <Header headerType={component_type.header_light} className="mt-6">
@@ -12,12 +12,16 @@ const Home = () => {
         </Header>
       </div>
 
-      <div className="col-start-2 row-start-2 flex flex-col items-center">
+      <div className="col-start-2 row-start-2 flex flex-col items-center gap-1">
         <p className="text-xl font-light tracking-[0.25em]">Developer</p>
         <p className="text-xl font-light tracking-[0.25em]">&amp;</p>
-        <p className="text-xl font-light tracking-[0.25em]">Design Enthusiast</p>
+        {/* <p className="hidden text-xl font-light tracking-[0.25em] lg:block">Design Enthusiast</p> */}
+
+        <div className="flex flex-col items-center lg:flex lg:flex-row lg:gap-[10px]">
+          <p className="text-xl font-light tracking-[0.25em]">Design</p>
+          <p className="text-xl font-light tracking-[0.25em]">Enthusiast</p>
+        </div>
       </div>
-      <div></div>
     </div>
   );
 };
