7a988b264b33fc4c0be30500f2970138793a17e2
Dibendu Saha <dibendu4@gmail.com>
2024-08-09 23:29:49 +0530

    Work in progress for About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Home from "./components/Home";
+import Home from "./components/home/Home";
 import "./App.scss";
+import About from "./components/about/About";
 
 const App = () => {
   return (
     <div className="mx-auto my-0 h-screen bg-gradient-to-r from-gray-200 to-yellow-300 px-10 py-20 md:px-20">
-      <Home />
+      {/* <Home /> */}
+      <About />
     </div>
   );
 };
