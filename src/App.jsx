85afbc0e37996e889e2bbc4effaf8aa0333d8965
Dibendu Saha <dibendu4@gmail.com>
2024-08-11 22:09:00 +0530

    Add gradient similar to existing website (to be applied)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import Home from "./components/home/Home";
-import "./App.scss";
 import AboutPersonal from "./components/about/AboutPersonal";
 import AboutProfessional from "./components/about/AboutProfessional";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.scss";
 
 const App = () => {
   return (
-    <div className="mx-auto my-0 min-h-screen bg-gradient-to-r from-gray-200 to-yellow-300 px-10 py-10 md:px-20 md:py-20">
+    // <div className="app-bg mx-auto my-0 min-h-screen bg-gradient-to-r from-[#e6e6e6] to-[#d5c455] px-10 py-10 md:px-20 md:py-20">
+    <div className="app-bg mx-auto my-0 min-h-screen bg-gradient-to-r from-gray-200 to-yellow-300 px-10 py-10 md:px-20 md:py-20">
+      {/* <div className="app-bg mx-auto my-0 min-h-screen px-10 py-10 md:px-20 md:py-20"> */}
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
