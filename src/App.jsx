06b714149d002532e449a4383a03786330b375ac
Dibendu Saha <dibendu4@gmail.com>
2024-08-12 21:31:00 +0530

    Refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ import "./App.scss";
 const App = () => {
   return (
     // <div className="app-bg mx-auto my-0 min-h-screen bg-gradient-to-r from-[#e6e6e6] to-[#d5c455] px-10 py-10 md:px-20 md:py-20">
-    <div className="app-bg mx-auto my-0 min-h-screen bg-gradient-to-r from-gray-200 to-yellow-300 px-10 py-10 md:px-20 md:py-20">
-      {/* <div className="app-bg mx-auto my-0 min-h-screen px-10 py-10 md:px-20 md:py-20"> */}
+    // <div className="app-bg mx-auto my-0 min-h-screen px-10 py-10 md:px-20 md:py-20">
+    <div className="mx-auto my-0 min-h-screen bg-gradient-to-r from-gray-200 to-yellow-300 px-10 py-10 md:px-20 md:py-20">
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
