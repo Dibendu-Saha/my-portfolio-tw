454da9f5d5ff10325cf4578636884aef738643ed
Dibendu Saha <dibendu4@gmail.com>
2024-08-14 21:43:13 +0530

    Add all mobile menu icons

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import homeIcon from "../../assets/img/menu/menu-home.svg";
+import aboutIcon from "../../assets/img/menu/menu-about.svg";
+import workIcon from "../../assets/img/menu/menu-work.svg";
+import xpIcon from "../../assets/img/menu/menu-xp.svg";
+import contactIcon from "../../assets/img/menu/menu-contact-1.svg";
 import { NavLink } from "react-router-dom";
 
 const Menu = () => {
   return (
-    <div className="fixed bottom-0 left-0 flex h-12 w-full items-center justify-center rounded-t-lg bg-[#574c3c] shadow-[#574c3c_0_0_20px_0] md:hidden">
+    <div className="fixed bottom-0 left-0 flex h-12 w-full items-center justify-evenly rounded-t-lg bg-[#574c3c] shadow-[#574c3c_0_0_20px_0] md:hidden">
       <NavLink to="/">
-        <img src={homeIcon} className="size-7" alt="" />
+        <img src={homeIcon} className="size-6" alt="" />
+      </NavLink>
+
+      <NavLink to="/about-personal">
+        <img src={aboutIcon} className="size-6" alt="" />
+      </NavLink>
+
+      <NavLink to="/about-professional">
+        <img src={workIcon} className="size-7" alt="" />
+      </NavLink>
+
+      <NavLink to="/">
+        <img src={xpIcon} className="size-6 -translate-y-0.5" alt="" />
+      </NavLink>
+
+      {/* <NavLink to="/">
+        <img src={xpIcon} className="size-6" alt="" />
+      </NavLink> */}
+
+      <NavLink to="/">
+        <img src={contactIcon} className="size-6 scale-110" alt="" />
       </NavLink>
     </div>
   );
