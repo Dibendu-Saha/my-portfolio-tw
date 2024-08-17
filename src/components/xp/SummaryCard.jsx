3a7a5ac720849777f905049a3d9f9731df9c41e4
Dibendu Saha <dibendu4@gmail.com>
2024-08-17 12:31:08 +0530

    Add className support for SummaryCard

--- src/components/xp/SummaryCard.jsx
+++ src/components/xp/SummaryCard.jsx
@@ -1,6 +1,18 @@
-const SummaryCard = ({ children, employerLogo, jobTitle, startDate, endDate, imgSize = "12", imageScale = "1" }) => {
+const SummaryCard = ({
+  children,
+  employerLogo,
+  jobTitle,
+  startDate,
+  endDate,
+  className,
+  imgSize = "12",
+  imageScale = "1",
+}) => {
+  const cn = className ?? "";
   return (
-    <div className="flex min-h-36 items-start gap-2 rounded-xl bg-gradient-to-r from-[#6b5d4a] px-2 py-3">
+    <div
+      className={`${cn} flex min-h-36 items-start gap-2 rounded-xl bg-gradient-to-r from-[#6b5d4a] px-2 py-3 lg:min-h-40 lg:gap-6 lg:p-4`.trim()}
+    >
       <div className="w-fit flex-shrink-0 rounded-full bg-slate-300 p-4">
         <img src={employerLogo} alt="Employer logo" className={`size-${imgSize} scale-[${imageScale}] md:size-16`} />
       </div>
