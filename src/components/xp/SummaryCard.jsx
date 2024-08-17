be562f4d354d322b83c786f9885e8c19644d4239
Dibendu Saha <dibendu4@gmail.com>
2024-08-17 13:59:13 +0530

    Update summary card design
    Update design from gradient to box with yellow background and brown border.

--- src/components/xp/SummaryCard.jsx
+++ src/components/xp/SummaryCard.jsx
@@ -11,7 +11,7 @@ const SummaryCard = ({
   const cn = className ?? "";
   return (
     <div
-      className={`${cn} flex min-h-36 items-start gap-2 rounded-xl bg-gradient-to-r from-[#6b5d4a] px-2 py-3 lg:h-44 lg:gap-6 lg:p-4`.trim()}
+      className={`${cn} flex min-h-36 items-start gap-2 rounded-xl border border-[#ad9676] bg-[#d5c455] px-2 py-3 shadow-lg lg:h-44 lg:gap-6 lg:p-4`.trim()}
     >
       <div className="w-fit flex-shrink-0 rounded-full bg-slate-300 p-4">
         <img src={employerLogo} alt="Employer logo" className={`size-${imgSize} scale-[${imageScale}] md:size-16`} />
@@ -21,7 +21,7 @@ const SummaryCard = ({
         <p className="text-slate-800">
           <strong>{jobTitle}</strong>
         </p>
-        <p className="text-slate-800 opacity-80">
+        <p className="text-left text-slate-800 opacity-70">
           {startDate} - {endDate}
         </p>
 
