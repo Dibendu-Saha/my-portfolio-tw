114085d3ee86cbad77ecd9241ff817605b3690b3
Dibendu Saha <dibendu4@gmail.com>
2024-08-21 20:07:59 +0530

    Add image gallery
    ...and AppComponent.jsx just has some formatting.

--- src/components/about/AboutPersonal.jsx
+++ src/components/about/AboutPersonal.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Button, Container, Footer, Heading, LeftPane, PageContent, RightPane } from "../../common/AppComponents";
 import { route } from "../../common/app-constants";
+import p1 from "../../assets/img/gallery/p-1.jpg";
+import p2 from "../../assets/img/gallery/p-2.jpg";
+import p3 from "../../assets/img/gallery/p-3.jpg";
+import p4 from "../../assets/img/gallery/p-4.jpg";
+import p5 from "../../assets/img/gallery/p-5.jpg";
+import p6 from "../../assets/img/gallery/p-6.jpg";
 
 const AboutPersonal = () => {
   return (
@@ -10,23 +16,29 @@ const AboutPersonal = () => {
       <PageContent>
         <LeftPane>
           <p>
-            Hi, I'm <strong>Dibendu Saha</strong>. Originally from the northeastern state of Assam, I now call Bangalore
-            home.
+            Hi, I'm <strong>Dibendu Saha</strong>. Originally from the northeastern state of Assam, I now call Bangalore home.
           </p>
 
           <p>
-            I'm fluent in four languages &#8212; English, Hindi, Assamese and Bengali, my native tongue. I have a
-            passion for cooking and love experimenting with new recipes for my loved ones, believing that nothing
-            touches the soul quite like wholesome food.
+            I'm fluent in four languages &#8212; English, Hindi, Assamese and Bengali, my native tongue. I have a passion for cooking and love experimenting
+            with new recipes for my loved ones, believing that nothing touches the soul quite like wholesome food.
           </p>
 
           <p>
-            Traveling is one of my greatest joys (like who doesn't love it?), and I'm an avid photography enthusiast. I
-            also enjoy biking and occasionally nowhere to be found on weekends. I'm also part of a professional rider's
-            groups. So, I'm always in for both solo trips and group rides.
+            Traveling is one of my greatest joys (like who doesn't love it?), and I'm an avid photography enthusiast. I also enjoy biking and occasionally
+            nowhere to be found on weekends. I'm also part of a professional rider's groups. So, I'm always in for both solo trips and group rides.
           </p>
         </LeftPane>
-        <RightPane></RightPane>
+        <RightPane>
+          <div className="mx-12 grid grid-cols-3 justify-items-center">
+            <img src={p1} className="transition-all duration-300 ease-out hover:scale-125" alt="" />
+            <img src={p2} className="transition-all duration-300 ease-out hover:scale-125" alt="" />
+            <img src={p3} className="transition-all duration-300 ease-out hover:scale-125" alt="" />
+            <img src={p4} className="transition-all duration-300 ease-out hover:scale-125" alt="" />
+            <img src={p5} className="transition-all duration-300 ease-out hover:scale-125" alt="" />
+            <img src={p6} className="transition-all duration-300 ease-out hover:scale-125" alt="" />
+          </div>
+        </RightPane>
       </PageContent>
 
       <Footer>
