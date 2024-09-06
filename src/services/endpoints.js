a96e96a855363eab8f0d84b0910992b5f7e99763
Dibendu Saha <dibendu4@gmail.com>
2024-09-06 17:16:33 +0530

    Add endpoints

---
+++ src/services/endpoints.js
@@ -0,0 +1,5 @@
+export const ENDPOINT = {
+    SEND_EMAIL: 'https://portfoliosendemailazurefunction.azurewebsites.net/api/SendEmail',
+    DOWNLOAD_CV: 'https://downloadcvazfn.azurewebsites.net/api/DownloadCv',
+    LOG_SITE_VISIT: 'https://portfoliologsitevisit.azurewebsites.net/api/LogSiteVisit'
+}
