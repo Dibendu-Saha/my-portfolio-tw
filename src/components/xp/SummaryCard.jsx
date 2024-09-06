f5b510488f71c407e1f6a6dd91440f2a28e64e5a
Dibendu Saha <65119411+Dibendu-Saha@users.noreply.github.com>
2024-09-07 01:33:47 +0530

    Download CV on click

--- src/components/xp/SummaryCard.jsx
+++ src/components/xp/SummaryCard.jsx
@@ -16,7 +16,6 @@ const SummaryCard = ({
 
   return (
     <div
-      onClick={actionable && (() => alert("download"))}
       className={`${cn} flex min-h-36 items-start gap-2 rounded-xl border border-[#ad9676] bg-[#e1dfdb] px-2 py-3 shadow-lg transition-all duration-200 ${actionable && "hover:scale-[1.02] hover:cursor-pointer hover:shadow-xl"} lg:h-44 lg:min-h-48 lg:gap-6 lg:p-4`.trim()}
     >
       <div className={`w-fit flex-shrink-0 rounded-lg bg-slate-300 p-4`}>
