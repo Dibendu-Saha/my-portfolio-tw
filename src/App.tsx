ffcd5ee8fc99b4c4045c3144cc1415f89694dba6
Dibendu Saha <65119411+Dibendu-Saha@users.noreply.github.com>
2024-12-07 00:13:43 +0530

    Initiate TypeScript migration
    + Add package 'typescript'.
    + Add config files.
    + Update build script.
    + Update main & App file to 'tsx' extension.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/home/Home";
 import AboutPersonal from "./components/about/AboutPersonal";
