5be48b3c6e3caf1f1809d0471f409ae40935e26a
Dibendu Saha <65119411+Dibendu-Saha@users.noreply.github.com>
2025-02-26 21:52:09 +0530

    Remove image hover

--- src/components/about/GalleryImage.tsx
+++ src/components/about/GalleryImage.tsx
@@ -1,5 +1,5 @@
 const GalleryImage: React.FC<{ src: string }> = ({ src }) => {
-  return <img fetchPriority="high" src={src} className={`rounded-full p-2 transition-all duration-100 hover:rounded-3xl`} alt="image" />;
+  return <img fetchPriority="high" src={src} className={`rounded-full p-2 transition-all duration-100`} alt="image" />;
 };
 
 export default GalleryImage;
