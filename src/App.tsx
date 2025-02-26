0988705cc637fed7bff480674eb0154574a7f64c
Dibendu Saha <65119411+Dibendu-Saha@users.noreply.github.com>
2025-02-26 19:59:22 +0530

    Test image optimization

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import AboutProfessional from "./components/about/AboutProfessional";
 import Menu from "./components/menu/Menu";
 import Contact from "./components/contact/Contact";
 import ExperienceHighlight from "./components/xp/ExperienceHighlight";
+import { useEffect } from "react";
+import { preloadGalleryImages } from "./utils/imagePreloader";
 
 import img1 from "./assets/img/gallery/p-1.jpg";
 import img2 from "./assets/img/gallery/p-2.jpg";
@@ -19,6 +21,12 @@ import "react-toastify/dist/ReactToastify.css";
 import "./App.scss";
 
 const App = () => {
+  useEffect(() => {
+    preloadGalleryImages()
+      .then(() => console.log("Gallery images preloaded successfully"))
+      .catch((error) => console.error("Error preloading gallery images:", error));
+  }, []);
+
   return (
     <div className="mx-auto my-0 min-h-screen bg-gradient-to-r from-gray-200 to-yellow-300 p-10 md:from-0% md:from-50% md:to-[#d5c455] md:to-100% md:to-50% md:p-20">
       <BrowserRouter>
